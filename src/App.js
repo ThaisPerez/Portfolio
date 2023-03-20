8219a8bfdeb61d3b2de55cd09b2c3358cc08283b
Thais Perez <thaisperezuchoa243@gmail.com>
2023-03-20 11:17:28 -0400

    alteracoes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Navbar />
+      <div className="ContentText">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá, meu nome é Thais, trabalho atualmente como técnica de garantia da qualidade. Gosto de
+          desafios novos para poder adquirir novos conhecimentos e sou apaixonada por tecnologia..
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+  function Navbar() {
+
+    return (
+      <div className="container">
+        <nav>
+          <a >Início</a>
+          <a>Sobre mim </a>
+        </nav>
+      </div>
+    );
+  }
+
+  export default Navbar;
