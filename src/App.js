23843a6c0f67625666bf2cf6eebfdf6ef4d43bf5
Thais Perez <thaisperezuchoa243@gmail.com>
2023-03-21 17:32:12 -0400

    alteracoes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 import Navbar from './components/Navbar';
-import Container from './components/Container'
-import Perfil from './assets/perfil.svg'
+import Container from './components/Container';
+import Perfil from './assets/perfil.svg';
+import ContainerExperiencias from './components/ContainerExperiencias';
+import Ipam from './assets/ipam.svg';
+import Uniron from './assets/uniron.svg';
+import SAMAR from './assets/samar.svg';
+import Coimbra from './assets/coimbra.svg';
+
 function App() {
   return (
     <div>
       <Navbar />
       <div className="Container">
-        <div className='Content'>
-        <p>
-          Olá, meu nome é Thais, trabalho atualmente como técnica de garantia da qualidade. Gosto de desafios novos para poder adquirir novos conhecimentos e sou apaixonada por tecnologia.
-        </p>
-        <div className='ContainerCard'>
-        <Container titulo="Redes de computadores" />   
-        <Container titulo="Informática  Básica" />   
-        <Container titulo="Sistemas Operacionais" />   
-        <Container titulo="React" />   
-        <Container titulo="CSS" />   
-        <Container titulo="HTML" />   
-        <Container titulo="Bootstrap" />   
-        <Container titulo="PHP" />   
-          <Container titulo="Selenium" />   
+        
+        <div className='ContainerHeader'>
+        <div className="Content">
+          <p>
+            Olá, meu nome é Thais, trabalho atualmente como técnica de garantia da qualidade. Gosto
+            de desafios novos para poder adquirir novos conhecimentos e sou apaixonada por
+            tecnologia.
+          </p>
+          <div className="ContainerCard">
+            <Container titulo="Redes de computadores" />
+            <Container titulo="Informática  Básica" />
+            <Container titulo="Sistemas Operacionais" />
+            <Container titulo="React" />
+            <Container titulo="CSS" />
+            <Container titulo="HTML" />
+            <Container titulo="Bootstrap" />
+            <Container titulo="PHP" />
+            <Container titulo="Selenium" />
+            </div>
           </div>
         </div>
-        <div className='Content-img'>
+        <div className="Content-img">
           <img src={Perfil} alt="" />
         </div>
+        <ContainerExperiencias
+          img={Ipam}
+          titulo="Estágio - CPD"
+          subtitulo="Manuntenção, formação, instalação, limpeza."
+        />
+        <ContainerExperiencias
+          img={Uniron}
+          titulo="Analista de redes e de Comunicação "
+          subtitulo="Manuntenção, formação, instalação, limpeza."
+        />
+        <ContainerExperiencias
+          img={SAMAR}
+          titulo="Estágio - CPD"
+          subtitulo="Manuntenção, formação, instalação, limpeza."
+        />
+        <ContainerExperiencias
+          img={Coimbra}
+          titulo="Técnico de qualidade"
+          subtitulo="Teste com cypress, selenium, montagem de cenarios "
+        />
       </div>
     </div>
   );
--- src/components/ContainerExperiencias/index.jsx
+++ src/components/ContainerExperiencias/index.jsx
@@ -3,10 +3,10 @@ import './styles.css'
 export default function Card(props) {
   return (
     <div className='Card'>
+      <img className='imgSuporte' src={props.img} alt=""></img>
       <span>{props.titulo}</span>
-      <div class="progress">  <div class="progress-value">70%</div></div>
-      
-    </div>
-    
+      <a>{props.subtitulo}</a>
+      </div>
+
   )
 }
