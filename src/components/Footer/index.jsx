c74cd1d249b26aaeea34974cf398055b028c43e4
Thais Perez <thaisperezuchoa243@gmail.com>
2023-03-30 17:17:23 -0400

    alteracoes no footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,18 @@
 import "./styles.css";
+import Linkedin from "./../../assets/linkedin.svg"
+import Whatsapp from "./../../assets/whatsapp.svg"
+
 
 export default function Footer (){
   return (
-    <div className=" footerContainer"></div>
+    <div className=" footerContainer">
+      <div className=" footerLinkedin">
+        <img src={Linkedin} alt="" />
+      </div>
+      <div className=" footerWhatsapp">
+        <img src={Whatsapp} alt="" />
+      </div>
+  
+    </div>
   );
 }
